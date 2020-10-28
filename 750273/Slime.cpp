885c2152967798951ecf1a035016caab70119687
allison61014 <allison61014@gmail.com>
2020-10-29 01:06:44 +0800

    史萊姆生態區

---
+++ 750273/Slime.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int N, T;
+    cin >> N >> T;
+
+    if (N != 1) { 
+        T /= N;
+        N /= N;
+    }
+
+    int day = 0;
+    int amount = T + N;
+    
+    while (amount != 1) {
+        amount /= 2;
+        day++;
+    }
+    
+    cout << day;
+}
