ba8a23c6fe92545453eadb16e2e01cce600924f1
papayaisnotafood <70697738+papayaisnotafood@users.noreply.github.com>
2020-10-28 16:10:56 +0800

    Update verification.cpp

--- 755001/verification.cpp
+++ 755001/verification.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
   int inputnum[9],max,max2,num1,num2;
-    for(int i;i <= 9; i++) {
-    cin >> inputnum[i];
-    }
+  cin >> inputnum[1];
+
   for (size_t j = 0; j <= 9; j++) {
-    if (inputnum[j]> max) {
-      max = inputnum[j];
+    if (inputnum[1][j]> max) {
+      max = inputnum[1][j];
     }
   }
   for (size_t l = 0; l <= 9; l++) {
-    if (inputnum[l]>=max2 || inputnum[l]<= max) {
-      max2 = inputnum[l];
+    if (inputnum[1][l]>=max2 || inputnum[1][l]<= max) {
+      max2 = inputnum[1][l];
     }
   }
   num1 = max*max + max2*max2;
-  num2 = inputnum[6]+inputnum[7]+inputnum[8];
+  num2 = inputnum[1][6]+inputnum[1][7]+inputnum[1][8];
   if (num1 == num2) {
     cout << "Good Morning!";
   }else {
