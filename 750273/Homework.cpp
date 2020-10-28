b330d069b8ef5965365330ddc872bfa3ac974c7c
allison61014 <allison61014@gmail.com>
2020-10-29 01:06:32 +0800

    小豪的回家作業

---
+++ 750273/Homework.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int R;
+    int A;
+    cin >> R >> A;
+
+    int ans = 0;
+    if (R > A) {
+        ans = R - A;
+        if (R > ans) {
+            cout << ans << "+" << A << "=" << R;
+        } else {
+            cout << A << "+" << ans << "=" << R;
+        }
+    } else if (R == A) {
+        A -= 3;
+        ans = R - A;
+        cout << A << "+" << ans << "=" << R;
+    } else {
+        ans = A - R;
+        if (A > ans) {
+            cout << ans << "+" << R << "=" << A;
+        } else {
+            cout << R << "+" << ans << "=" << A;
+        }
+    }
+}
